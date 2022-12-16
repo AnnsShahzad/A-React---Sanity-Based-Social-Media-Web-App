b577c091a2ef3c797e1f41b7426a550bd0d74023
Anns <annskhokhar@gmail.com>
2022-12-16 17:08:16 +0500

    Sanity And Tailwind Implemented

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from "./user";
+import pin from "./pin";
+import postedBy from "./postedBy";
+import comment from "./comment";
+import save from "./save";
+
+export const schemaTypes = [pin, user, postedBy, comment, save]
